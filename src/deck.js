6a1dca04715cd75ebd5066c0ef9a0551a0755456
tbanish <tombanish@gmail.com>
2021-03-21 19:53:25 -0400

    in renderDeck() - create cardContainer div and cardList ol, give div classname, append cardContainer to div and cardList to cardContainer

--- src/deck.js
+++ src/deck.js
@@ -9,12 +9,21 @@ class Deck {
     const deckContainer = document.querySelector(".deck-container")
     const div = document.createElement("div")
     const subjectPTag = document.createElement("p")
+    const cardContainer = document.createElement("div")
+    const cardList = document.createElement("ol")
 
     div.id = `${this.id}`
+    div.classList.add("deck-div")
     subjectPTag.innerHTML = `${this.subject}`
     subjectPTag.classList.add("deck-subject")
+    cardContainer.classList.add("card-container")
+    cardContainer.id = `${this.subject} cards`
+    cardList.classList.add("card-list")
+
     div.appendChild(subjectPTag)
     deckContainer.appendChild(div)
+    div.appendChild(cardContainer)
+    cardContainer.appendChild(cardList)
   }
 }
 
