c0cbb1daae540fde113cc1881b9f795c26c01bf2
tbanish <tombanish@gmail.com>
2021-03-21 18:52:25 -0400

    build card class

---
+++ src/card.js
@@ -0,0 +1,11 @@
+class Card {
+  constructor(id, question, answer, deckId) {
+    this.id = id
+    this.question = question
+    this.answer = answer
+    this.deckId = deckId
+    Card.all.push(this)
+  }
+}
+
+Card.all = [];
