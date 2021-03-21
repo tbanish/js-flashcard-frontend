574d04b3b0ad08916bb093892895dfd91991dec1
tbanish <tombanish@gmail.com>
2021-03-21 19:11:30 -0400

    build renderDeck() function in deck class

--- src/deck.js
+++ src/deck.js
@@ -4,6 +4,18 @@ class Deck {
     this.subject = subject
     Deck.all.push(this)
   }
+
+  renderDeck() {
+    const deckContainer = document.querySelector(".deck-container")
+    const div = document.createElement("div")
+    const subjectPTag = document.createElement("p")
+
+    div.id = `${this.id}`
+    subjectPTag.innerHTML = `${this.subject}`
+    subjectPTag.classList.add("deck-subject")
+    div.appendChild(subjectPTag)
+    deckContainer.appendChild(div)
+  }
 }
 
 Deck.all= [];
