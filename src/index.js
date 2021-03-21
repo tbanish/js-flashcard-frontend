cc28a9ebb0700543b7f189fc8ce52ed8ed4acd58
tbanish <tombanish@gmail.com>
2021-03-21 18:36:19 -0400

    add event listener on DOMContentLoaded and build loadDecks() function that fetches and creates deck objects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const decksEndpoint = "http://localhost:3000/api/v1/decks"
 const cardsEndpoint = "http://localhost:3000/api/v1/cards"
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadDecks();
+})
+
+function loadDecks() {
+  fetch(decksEndpoint)
+  .then(resp => resp.json())
+  .then(decks => {
+    for (const deck of decks.data) {
+      const subject = deck.attributes.subject
+      const newDeck = new Deck(deck.id, subject)
+    }
+  })
+}
