0dc2c9923a542eec53df7c7ed226abd9a3e83f8a
tbanish <tombanish@gmail.com>
2021-06-04 13:22:46 -0400

    add const testsEndpoint to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const decksEndpoint = "http://localhost:3000/api/v1/decks"
 const cardsEndpoint = "http://localhost:3000/api/v1/cards"
+const testsEndpoint = "http://localhost:3000/api/v1/tests"
 
 document.addEventListener('DOMContentLoaded', () => {
   const newDeckForm = document.getElementById("new-deck-form")
