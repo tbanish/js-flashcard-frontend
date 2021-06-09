d3fd9bd05e577b6def6ab0bbab4abe09d9f61c30
tbanish <tombanish@gmail.com>
2021-06-09 15:53:08 -0400

    build renderTest() - add header to test box and start and clear buttons;

--- src/test.js
+++ src/test.js
@@ -8,6 +8,33 @@ class Test {
     this.deckId = deckId
     Test.all.push(this)
   }
+
+  static renderTest(subject) {
+    if (document.querySelector(".test-header")) {
+      document.querySelector(".test-header").remove()
+      document.querySelector(".btn.start").remove()
+      document.querySelector(".btn.clear").remove()
+    }
+
+    if (subject === "--select a deck to test your knowledge--") {
+      return
+    }
+
+    const testBox = document.querySelector(".test-box")
+    const header = document.createElement("h3")
+    const startButton = document.createElement("button")
+    const clearButton = document.createElement("button")
+
+    startButton.innerText = "Start Test"
+    startButton.className = "btn start"
+    clearButton.innerText = "Clear Test"
+    clearButton.className = "btn clear"
+    header.innerText = subject
+    header.className = "test-header"
+    testBox.appendChild(header)
+    testBox.appendChild(startButton)
+    testBox.appendChild(clearButton)
+  }
 }
 
 Test.all = [];
