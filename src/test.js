ace4a21c8f5a3716422c320a640292c23a558499
tbanish <tombanish@gmail.com>
2021-06-15 19:15:05 -0400

    remove static renderTest() function; add this.correctAnswers and this.incorrectAnswers to constructor and set both equal to empty arrays;

--- src/test.js
+++ src/test.js
@@ -7,28 +7,8 @@ class Test {
     this.incorrectIds = incorrectIds
     this.deckId = deckId
     Test.all.push(this)
-  }
-
-  static renderTest(subject) {
-    if (document.querySelector(".test-header")) {
-      document.querySelector(".test-header").remove()
-      document.querySelector(".btn.start").remove()
-    }
-
-    if (subject === "--select a deck to test your knowledge--") {
-      return
-    }
-
-    const testBox = document.querySelector(".test-box")
-    const header = document.createElement("h3")
-    const startButton = document.createElement("button")
-
-    startButton.innerText = "Start Test"
-    startButton.className = "btn start"
-    header.innerText = subject
-    header.className = "test-header"
-    testBox.appendChild(header)
-    testBox.appendChild(startButton)
+    this.correctAnswers = []
+    this.incorrectAnswers = []
   }
 }
 
