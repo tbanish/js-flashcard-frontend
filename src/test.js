dd7531a4d614f0eb5614d03700404db47f9ad724
tbanish <tombanish@gmail.com>
2021-06-16 17:00:17 -0400

    add date to constructor; build findTestsByDeckId() static method;

--- src/test.js
+++ src/test.js
@@ -1,14 +1,20 @@
 class Test {
-  constructor(id, duration, score, correctIds, incorrectIds, deckId) {
+  constructor(id, duration, score, correctIds, incorrectIds, deckId, date) {
     this.id = id
     this.duration = duration
     this.score = score
     this.correctIds = correctIds
     this.incorrectIds = incorrectIds
     this.deckId = deckId
+    this.date = date
     Test.all.push(this)
     this.correctAnswers = []
     this.incorrectAnswers = []
+  }
+
+  static findTestsByDeckId(deckId) {
+    const tests = Test.all.filter(test => test.deckId === deckId)
+    return tests
   }
 }
 
